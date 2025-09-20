0afe85fd06356f88bda15fef8283c04eec893ec9
AshishFullStackDev <ashishnaik1386@gmail.com>
2025-09-20 10:02:52 +0530

    Add files via upload

---
+++ js/produjctData.js
@@ -0,0 +1,26 @@
+productData = [
+    {
+        name:"Laptop",
+        price:40000,
+    },
+    {
+        name:"Smartphone",
+        price:69999,
+        
+    },
+    {
+        name:"Smart TV",
+        price:74999,
+        
+    },
+    {
+        name: "Smartwatch",
+        price:45999,
+        
+    },
+    {
+        name:"Wireless Headphones",
+        price:1999,
+        
+    },
+]
